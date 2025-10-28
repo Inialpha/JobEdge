823a830afaac6b25da402a0da18821d6603277f8
Inialpha <inimfonebong001@gmail.com>
2025-10-28 12:30:46 +0000

    Add https://jobedge.onrender.com to allowed host

--- backend/JobEdgeApi/settings.py
+++ backend/JobEdgeApi/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7v_7-u*1*7()b-77k4v^6*v!qp18pf4(*$r@dsrer9!qiy@a!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jobedge.onrender.com"]
 
 
 # Application definition
